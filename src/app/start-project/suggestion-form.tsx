c816e0099a9e6ce23197a8be3dff0425dfd93dc4
Energy burner of motivation <prakashbabuparamhans729srmt@gmail.com>
2025-12-20 04:54:27 +0000

    Try fixing this error: `Runtime TypeError: (0 , {imported module [projec

--- src/app/start-project/suggestion-form.tsx
+++ src/app/start-project/suggestion-form.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { useActionState, useFormStatus } from 'react';
+import { useActionState } from 'react';
+import { useFormStatus } from 'react-dom';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
