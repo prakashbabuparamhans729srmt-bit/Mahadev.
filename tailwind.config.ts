886e481a49714cf4a24ef41407cc7b675b19500c
Energy burner of motivation <prakashbabuparamhans729srmt@gmail.com>
2025-12-22 08:08:04 +0000

    bhai abhi bhi nahi hata

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,6 @@ export default {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
       fontFamily: {
         body: ['PT Sans', 'sans-serif'],
