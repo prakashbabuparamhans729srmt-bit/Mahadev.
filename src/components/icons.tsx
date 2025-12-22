54a075ccfe7838963829443bff781279d3faa878
Energy burner of motivation <prakashbabuparamhans729srmt@gmail.com>
2025-12-22 09:55:03 +0000

    imaze me jo logo dikh raha hai use or beautifull kar ke ,add kar do

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -7,16 +7,27 @@ export const Icons = {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      strokeWidth="2"
+      strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M12.22 2h-4.44L4 10v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V10L16.22 2z" />
-      <path d="M8.29 14.29L6 12l2.29-2.29" />
-      <path d="M15.71 14.29L18 12l-2.29-2.29" />
-      <path d="M12 6V2" />
-      <path d="M12 18v-4" />
+      {/* Outer drop shape */}
+      <path d="M12 22C7.58172 22 4 17.94 4 12.5C4 7.06 12 2 12 2s8 5.06 8 10.5C20 17.94 16.4183 22 12 22Z" strokeWidth="2" />
+      
+      {/* 'M' shape */}
+      <path d="M9 11L10.5 8L12 11L13.5 8L15 11" />
+      
+      {/* 'D' shape */}
+      <path d="M9 13v4h2.5c1.5 0 2.5-1 2.5-2s-1-2-2.5-2H9Z" />
+
+      {/* 'C' shape */}
+      <path d="M15.5 17c1.5 0 2.5-1 2.5-2s-1-2-2.5-2h-1" />
+
+      {/* Red accent diamonds */}
+      <path d="M12 5.5l-1-1 1-1 1 1 -1 1z" fill="red" stroke="red" />
+      <path d="M10.5 7.5l-0.5-0.5 0.5-0.5 0.5 0.5 -0.5 0.5z" fill="red" stroke="red" />
+      <path d="M13.5 7.5l-0.5-0.5 0.5-0.5 0.5 0.5 -0.5 0.5z" fill="red" stroke="red" />
     </svg>
   ),
 };
