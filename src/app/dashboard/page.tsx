0c5c7d30b9893dd8d919bd86a56d175bb52e8599
Energy burner of motivation <prakashbabuparamhans729srmt@gmail.com>
2025-12-22 06:48:12 +0000

    kya fuul screen tab me nahi dik sakta hai

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -102,7 +102,7 @@ export default function DevPortalDashboard() {
   }, []);
 
   return (
-    <div className="container mx-auto p-4 md:p-6 lg:p-8">
+    <div className="p-4 md:p-6 lg:p-8">
       <div className="flex flex-col gap-6">
         <div className='flex justify-between items-center'>
              <h1 className="text-2xl font-bold font-headline">
