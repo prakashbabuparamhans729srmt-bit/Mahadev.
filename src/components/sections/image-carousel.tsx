e3db738b38d5af369e350b26961a4973384b4876
Energy burner of motivation <prakashbabuparamhans729srmt@gmail.com>
2025-12-23 11:33:48 +0000

    jo page view ke 20 imaze hai wo laptop me chota dikhai de taha hai use k

--- src/components/sections/image-carousel.tsx
+++ src/components/sections/image-carousel.tsx
@@ -32,7 +32,7 @@ export default function ImageCarouselSection() {
         >
           <CarouselContent>
             {carouselImages.map((image) => (
-              <CarouselItem key={image.id} className="md:basis-1/2 lg:basis-1/4">
+              <CarouselItem key={image.id} className="basis-full sm:basis-1/2 lg:basis-1/3 xl:basis-1/4">
                 <div className="p-1">
                   <div className="overflow-hidden rounded-lg aspect-video relative">
                     <Image
