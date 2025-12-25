30ed3a9db86b52206d902162b5fe8f0cf42bf126
Energy burner of motivation <prakashbabuparamhans729srmt@gmail.com>
2025-12-25 11:26:00 +0000

    [2025-12-25T11:19:50.077Z]  @firebase/firestore: Firestore (11.9.0): Web

--- next.config.ts
+++ next.config.ts
@@ -13,12 +13,12 @@ const securityHeaders = [
     key: 'Content-Security-Policy',
     value: `
       default-src 'self';
-      script-src 'self' 'unsafe-inline' 'unsafe-eval';
+      script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://www.gstatic.com;
       style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-      img-src 'self' https://images.unsplash.com https://picsum.photos;
+      img-src 'self' https://images.unsplash.com https://picsum.photos https://*.googleusercontent.com;
       font-src 'self' https://fonts.gstatic.com;
-      connect-src 'self' wss: *.firebaseapp.com *.googleapis.com;
-      frame-src 'self' *.firebaseapp.com;
+      connect-src 'self' wss: *.firebaseapp.com *.googleapis.com https://firestore.googleapis.com https://www.googleapis.com https://securetoken.googleapis.com *.cloudworkstations.dev;
+      frame-src 'self' *.firebaseapp.com https://*.firebaseapp.com;
     `.replace(/\s{2,}/g, ' ').trim()
   }
 ];
