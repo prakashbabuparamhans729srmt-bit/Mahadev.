c78c22afba08647f2b8173a60f22a939201fc4f6
Energy burner of motivation <prakashbabuparamhans729srmt@gmail.com>
2025-12-25 11:21:19 +0000

    orm field element has neither an id nor a name attribute. This might pre

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -6,8 +6,6 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<'tex
   ({className, ...props}, ref) => {
     return (
       <textarea
-        id={props.id}
-        name={props.name}
         className={cn(
           'flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
           className
